dc8ec1067e7f8830f4af86061acfbaac105366e8
kabir4874 <kabir.cse.bd@gmail.com>
2025-02-25 20:04:12 +0600

    update

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { dbConnect } = require("./utils/db");
 const app = express();
-const port = 3000;
 
 dotenv.config();
+const port = process.env.PORT || 8800;
 
 app.use(bodyParser.json());
-
-mongoose.connect(process.env.DB_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Database"));
-
-app.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  await User.findByIdAndDelete(id);
-  res.json("Delete successfully");
-});
-
-//TODO:  this
-
-app.listen(port, () => {
-  console.log(`Server is running on :${port}`);
-});
 
 const { Schema, model } = mongoose;
 const userSchema = new Schema({
@@ -33,3 +17,24 @@ const userSchema = new Schema({
   email: String,
 });
 const User = model("User", userSchema);
+
+dbConnect();
+
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json("Delete successfully");
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting user", error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on :${port}`);
+});
