1e93309a5297334093593921980b754d48fcb0cb
kabir4874 <kabir.cse.bd@gmail.com>
2025-03-02 15:26:15 +0600

    client part

---
+++ lib/pages/bottomnav.dart
@@ -0,0 +1,59 @@
+import 'package:client/pages/home.dart';
+import 'package:client/pages/order.dart';
+import 'package:client/pages/profile.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  late List<Widget> pages;
+  late Home homePage;
+  late Order orderPage;
+  late Profile profilePage;
+  int currentTabIndex = 0;
+
+  @override
+  void initState() {
+    homePage = Home();
+    orderPage = Order();
+    profilePage = Profile();
+    pages = [homePage, orderPage, profilePage];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+          height: 65,
+          backgroundColor: Colors.white,
+          color: Colors.black,
+          animationDuration: Duration(microseconds: 500),
+          onTap: (int index) {
+            setState(() {
+              currentTabIndex = index;
+            });
+          },
+          items: [
+            Icon(
+              Icons.home_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.person_2_outlined,
+              color: Colors.white,
+            ),
+          ]),
+    );
+  }
+}
