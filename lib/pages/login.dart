638b93464a73bd7fb76a6e5a63e1f882daa1f83e
kabir4874 <kabir.cse.bd@gmail.com>
2025-03-12 19:46:20 +0600

    update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -47,6 +47,51 @@ class _LoginState extends State<Login> {
                 decoration: InputDecoration(
                     border: InputBorder.none, hintText: 'Email'),
               ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Password',
+              style: AppWidget.semiboldTextFieldStyle(),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              decoration: BoxDecoration(
+                  color: Color(0xfff4f5f9),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: 'Password'),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 30),
+                padding: EdgeInsets.all(18),
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Text(
+                  'LOGIN',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
             )
           ],
         ),
