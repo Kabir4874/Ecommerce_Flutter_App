69b9cabedc331b5a8360347b4f805b157b8b383b
kabir4874 <kabir.cse.bd@gmail.com>
2025-03-18 22:02:50 +0600

    update

---
+++ lib/services/shared_pref.dart
@@ -0,0 +1,28 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferenceHelper {
+  static String userIdKey = "USER_ID_KEY";
+  static String userNameKey = "USER_NAME_KEY";
+  static String userEmailKey = "USER_EMAIL_KEY";
+  static String userImageKey = "USER_Image_KEY";
+
+  Future<bool> saveUserId(String getUserId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userIdKey, getUserId);
+  }
+
+  Future<bool> saveUserName(String getUserName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userNameKey, getUserName);
+  }
+
+  Future<bool> saveUserEmail(String getUserEmail) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userEmailKey, getUserEmail);
+  }
+
+  Future<bool> saveUserImage(String getUserImage) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userImageKey, getUserImage);
+  }
+}
