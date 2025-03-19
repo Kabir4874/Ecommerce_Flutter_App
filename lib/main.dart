6fe0bce05c9edd4896d2348b76fb753a5d7259cf
kabir4874 <kabir.cse.bd@gmail.com>
2025-03-19 20:27:40 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:client/pages/register.dart';
+import 'package:client/admin/admin_login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Register(),
+      home: AdminLogin(),
     );
   }
 }
